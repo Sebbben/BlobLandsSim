0ffba398701d237134dfe8320b988c4df7754534
BuildTools <unconfigured@null.spigotmc.org>
2022-05-04 12:33:47 +0200

    smooth camera movement

--- camera.py
+++ camera.py
@@ -2,6 +2,7 @@ class Camera:
     def __init__(self):
         self.pos = [0,0]
         self.target = self.pos.copy()
+        self.smoothness = 1/25
         
     def setTarget(self, target):
         self.target = target 
@@ -11,4 +12,7 @@ class Camera:
         self.target[1] += y
 
     def update(self):
-        self.pos = self.target
+        moveX = (self.target[0]-self.pos[0])*self.smoothness
+        moveY = (self.target[1]-self.pos[1])*self.smoothness
+        self.pos[0] += moveX
+        self.pos[1] += moveY
